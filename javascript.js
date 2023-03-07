ba013a1f1c3ab539832fde44342c593dd5ccce1a
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-03-06 19:30:18 -0800

    Create random R P S comp choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
 console.log(`Hello World`);
+
+let i = Math.round((Math.random() * 100));   //create variable i and set to random integer 1-100. 
+if (i >= 66) {
+    compChoice = 'rock';
+}
+else if (i >= 33) {
+    compChoice = 'paper';
+}
+else {
+    compChoice = 'scissors';
+}
+
+
+console.log(i);
+console.log(compChoice);
+
+
+
+
+
+      
