c909c21195e4e45ec3a22188715b8b6a19a0f89a
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-03-14 12:16:15 -0700

    Start over

--- javascript.js
+++ javascript.js
@@ -1,114 +1,46 @@
-console.log(`Welcome to rock paper scissors. `);
+//starting over:
 
-let rock = 'Rock';
-let paper = 'Paper';
-let scissors = 'Scissors';
-let computerChoice;
-let playerChoice;
-let scoreComputer = 0;
-let scorePlayer = 0;
+console.log(`Welcome to Rock, Paper, Scissors Game`);
 
 
+const choices = [`rock`,`paper`,`scissors`]
 
-function computerPlays() {
-    let i = Math.round((Math.random() * 100));
-    if (i >= 66 ) {       
-        let computerChoice = rock;
-     console.log(`Computer's choice: ` + rock);
-     return computerChoice;
-
-    } else if (i >= 33) {
-        let computerChoice = paper;
-        console.log(`Computer's choice: ` + paper);
-        return computerChoice;
-
-    } else {
-        let computerChoice = scissors;
-        console.log(`Computer's choice: ` + scissors);
-        return computerChoice;
-
-    }
+function game(){
+    playRound(); 
+        //get computer choice
+        //get player choice
+        //compare choices
+        //adjust score
 
 }
 
-
-function playerPlays() {                                                                        //Create function to ask player for their choice
-    let playerInput = prompt("Your choice! Please enter rock, paper, or scissors.");            //Create variable playerInput and set it equal to the answer to the questions
-    let inputUpperCase =  playerInput.toUpperCase(); 
-    //String(inputUpperCase);                
-    if (inputUpperCase === 'ROCK') {
-        playerChoice = rock;
-    }
-    else if (inputUpperCase === 'PAPER') {
-        playerChoice = paper;
-    }
-    else if (inputUpperCase === 'SCISSORS') {
-        playerChoice = scissors;
-    } 
-    console.log(`Player's choice: ` + playerChoice);
-    return playerChoice;
+function playRound() {
+    const computerChoice = computerPlays();
+    console.log(`Computer's choice: ${computerChoice}`);
+    const playerChoice = playerPlays();
+    console.log(playerChoice);
+    //if tie, try again. If not, continue to compare
 }
 
-
-let computerTurn = computerPlays();
-let playerTurn = playerPlays();
-
-
-if ((computerTurn == rock) && (playerTurn == rock)) {
-    console.log(`rock and rock, it's a tie!`);
-}
-else if ((computerTurn == paper) && (playerTurn == paper)) {
-    console.log(`paper and paper, it's a tie!`);
-}
-else if ((computerTurn == scissors) && (playerTurn == scissors)) {
-    console.log(`scissors and scissors, it's a tie!`);
-}
-
-//above scenarios tie
-
-else if ((computerTurn == rock) && (playerTurn == paper)) {
-    console.log(`Congratulations! Paper beats rock, player wins`);
-}
-
-else if ((computerTurn == rock) && (playerTurn == scissors)) {
-    console.log(`Sorry, rock beats scissors. You lose. `);
-}
-
-//above situations computer plays rock (not tie)
-
-else if ((computerTurn == paper) && (playerTurn == scissors)) {
-    console.log(`Congratulations! Scissors beat paper, player wins`);
-}
-
-else if ((computerTurn == paper) && (playerTurn == rock)) {
-    console.log(`Sorry, paper beats rock. You lose. `);
-}
-
-//above situations computer plays paper (not tie)
-
-else if ((computerTurn == scissors) && (playerTurn == rock)) {
-    console.log(`Congratulations! rock beat scissors, player wins`);
-}
-
-else if ((computerTurn == scissors) && (playerTurn == paper)) {
-    console.log(`Sorry, scissors beats paper. You lose. `);
-}
-
-else {
-    console.log(`something went wrong, you probably miss-spelled your input`);
-}
-
-
-
-// The below code block will compare reuslts and declare if it was a tie or not. I'm putting that on hold to try to compare for result. 
-
-/*
-if (computerTurn == playerTurn) {
-    console.log(`it's a tie!`)
-}
-else {
-    console.log(`It wasn't a tie.... (calculating winner)`);
+function computerPlays(){
+    return choices[Math.floor(Math.random()*choices.length)]
     
 }
-*/
 
+function playerPlays(){
+    let input = prompt(`Your turn! Type rock, paper, or scissors:`);
+    console.log(`Player typed: ${input}`);
+    input = input.toLowerCase();
+    console.log(`Player's input to lower case is: ${input}`);
+    if (choices.includes(input)) {
+    }
+    else{
+        let input =prompt(`Check your spelling and try again.`)
+        
+    }
+    return input;
+
+}
+
+
+playRound();
