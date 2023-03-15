af53469e0a8a15368df33ab15750e1439045e295
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-03-14 17:13:40 -0700

    Modify playRound function

--- javascript.js
+++ javascript.js
@@ -18,8 +18,18 @@ function playRound() {
     const computerChoice = computerPlays();
     console.log(`Computer's choice: ${computerChoice}`);
     const playerChoice = playerPlays();
-    console.log(playerChoice);
-    //if tie, try again. If not, continue to compare
+    console.log(`Player's choice: ${playerChoice}`);
+
+    if (computerChoice == playerChoice){
+        console.log(`it's a tie!`);
+    }
+   else {
+    console.log(`it isn't a tie`);
+   }
+   let result = (`${computerChoice}, ${playerChoice}`);
+   console.log(result);
+   return result;
+
 }
 
 function computerPlays(){
@@ -29,9 +39,7 @@ function computerPlays(){
 
 function playerPlays(){
     let input = prompt(`Your turn! Type rock, paper, or scissors:`);
-    console.log(`Player typed: ${input}`);
     input = input.toLowerCase();
-    console.log(`Player's input to lower case is: ${input}`);
     while (choices.includes(input) == false){
         input =prompt(`Try again and check your spelling`);
     }
@@ -41,3 +49,8 @@ function playerPlays(){
 
 
 playRound();
+
+
+
+
+//make a function that checks if it's a tie-then replays the match if it is?
