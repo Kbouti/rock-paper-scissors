a8ad4144f6551f7144fe1fd1dc1a306fd6cd2381
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-03-30 14:28:21 -0700

    Play game and report result in conle.log

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-const playerScore = 0
-const computerScore = 0
+let playerScore = 0
+let computerScore = 0
 
 const rockButton_button =document.getElementById(`rock`);
 const paperButton_button =document.getElementById(`paper`);
@@ -17,73 +17,64 @@ function computerplays(){
 function playRound(playerChoice){
     let computerChoice = computerplays();
     if (playerChoice == computerChoice){
-        let  message = `It's a tie!`
+        let  message = `It's a tie! The score is ${playerScore} - ${computerScore}`
         let result = `tie`;
+        console.log(message);
         return result;
     }
     else if (playerChoice == `r` && computerChoice == `p`){
-        let  message = `Paper beats rock. Computer wins`
+        let  message = `Paper beats rock. Computer wins. The score is ${playerScore} - ${computerScore}`
         computerScore += 1;
         let result = `computer`;
+        console.log(message);
         return result;
     }
-
-
     else if (playerChoice == `r` && computerChoice == `s`){
-        let  message = `Rock beats scissors, you win!`
+        let  message = `Rock beats scissors, you win! The score is ${playerScore} - ${computerScore}`
         playerScore += 1;
         let result = `player`;
+        console.log(message);
         return result;
     }
-
     else if (playerChoice == `p` && computerChoice == `s`){
-        let  message = `Scissors cuts paper. Computer wins`
+        let  message = `Scissors cuts paper. Computer wins. The score is ${playerScore} - ${computerScore}`
         computerScore += 1;
         let result = `computer`;
+        console.log(message);
         return result;
     }
-
     else if (playerChoice == `s` && computerChoice == `p`){
-        let  message = `Scissors beats paper, you win!`
+        let  message = `Scissors beats paper, you win! The score is ${playerScore} - ${computerScore}`
         playerScore += 1;
         let result = `player`;
+        console.log(message);
         return result;
     }
-
     else if (playerChoice == `p` && computerChoice == `r`){
-        let  message = `rock beats paper, somputer wins`
+        let  message = `rock beats paper, somputer wins. The score is ${playerScore} - ${computerScore}`
        computerScore += 1;
         let result = `computer`;
+        console.log(message);
         return result;
     }
-
     else if (playerChoice == `s` && computerChoice == `r`){
-        let  message = `Rock beats scissors, computer wins!`
+        let  message = `Rock beats scissors, computer wins! The score is ${playerScore} - ${computerScore}`
         computerScore += 1;
         let result = `computer`;
+        console.log(message);
         return result;
     }
-
-
-
-
-
-
-
-
-
-
 }
 
 
 rockButton_button.addEventListener(`click`, function(){
-    
+    playRound(`r`);
 })
 
 paperButton_button.addEventListener(`click`, function(){
-      
+    playRound(`p`);
 })
 
 scissorsButton_button.addEventListener(`click`, function(){
-    
+    playRound(`s`);
 })
