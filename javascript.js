38cea3371ff49f838add6b6d2b19af4fad48d767
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-03-30 12:55:40 -0700

    Define variables in the DOM

---
+++ javascript.js
@@ -0,0 +1,23 @@
+const playerScore = 0
+const computerScore = 0
+
+const rockButton_button =document.getElementById(`rock`);
+const paperButton_button =document.getElementById(`paper`);
+const scissorsButton_button =document.getElementById(`scissors`);
+const actionStatement_h3 =document.getElementById(`action-statement`);
+
+const playerScore_div =document.getElementById(`playerScore`);
+const computerScore_div =document.getElementById(`computerScore`);
+
+
+rockButton_button.addEventListener(`click`, function(){
+    
+})
+
+paperButton_button.addEventListener(`click`, function(){
+      
+})
+
+scissorsButton_button.addEventListener(`click`, function(){
+    
+})
