67918f4219897bc0e68af2218a9615911b23978c
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-04-03 17:57:56 -0700

    Update score and message

--- javascript.js
+++ javascript.js
@@ -14,11 +14,29 @@ let computerScore = 0
 const rockButton_button =document.getElementById(`rock`);
 const paperButton_button =document.getElementById(`paper`);
 const scissorsButton_button =document.getElementById(`scissors`);
-const actionStatement_h3 =document.getElementById(`action-statement`);
+const actionStatement_h3 =document.getElementById(`actionStatement`);
 const playerScore_div =document.getElementById(`playerScore`);
 const computerScore_div =document.getElementById(`computerScore`);
 
 const choices = [`r`, `p`, `s`]
+
+function choiceToWord(choice){
+    if (choice === `r`){
+        return `Rock`;
+    }
+    else if (choice === `p`){
+        return `Paper`;
+    }
+    else if (choice === `s`){
+        return `Scissors`;
+    }
+}
+
+function winMessage(playerChoice, computerChoice){
+    let message = `${choiceToWord(playerChoice)} beats ${choiceToWord(computerChoice)}. You win! `;
+    actionStatement_h3.innerHTML = message;
+}
+
 
 function computerplays(){
     return choices[Math.floor(Math.random()*choices.length)];
@@ -45,22 +63,60 @@ function playRound(playerChoice){
     let computerChoice = computerplays();
     switch (playerChoice + computerChoice){
         case "rs":
+            let winMessage1 = `Rock beats scissors, you win!`
+            playerScore++
+            playerScore_div.innerHTML = playerScore;
+            actionStatement_h3.innerHTML = winMessage1;
+            break;
+
         case "sp":
+            let winMessage2 = `Scissors beats paper, you win!`
+            playerScore++
+            playerScore_div.innerHTML = playerScore;
+            actionStatement_h3.innerHTML = winMessage2;
+            break;
+            
         case "pr":
-            win();
+            let winMessage3 = `Paper beats rock, you win!`
+            playerScore++
+            playerScore_div.innerHTML = playerScore;
+            actionStatement_h3.innerHTML = winMessage3;
+            break;
 
             break;
 
         case "sr":
+            let loseMessage1 = `Scissors lost to rock, you lose...`
+            computerScore++
+            computerScore_div.innerHTML = computerScore;
+            actionStatement_h3.innerHTML = loseMessage1;
+            break;
+
         case "ps":
+            let loseMessage2 = `Paper lost to scissors, you lose...`
+            computerScore++
+            computerScore_div.innerHTML = computerScore;
+            actionStatement_h3.innerHTML = loseMessage2;
+            break;
         case "rp":
-            lose();
+            let loseMessage3 = `Rock lost to paper, you lose...`
+            computerScore++
+            computerScore_div.innerHTML = computerScore;
+            actionStatement_h3.innerHTML = loseMessage3;
             break;
      
         case "ss":
+            let tieMessage1 = `You both picked scissors, it's a tie. Try again.`;
+            actionStatement_h3.innerHTML = tieMessage1;
+            break;
+
         case "rr":
+            let tieMessage2 = `You both picked rock, it's a tie. Try again.`;
+            actionStatement_h3.innerHTML = tieMessage2;
+            break;
         case "pp":
-            draw();
+            let tieMessage3 = `You both picked paper, it's a tie. Try again.`;
+            actionStatement_h3.innerHTML = tieMessage3;
             break;
     }
 }
